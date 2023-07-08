c91911ab42b2bb4e8b0c512640ef72ee6062c515
BabakMotevalli <babak.motevalli@outlook.com>
2023-07-08 19:13:27 +0330

    Initialize

---
+++ Ekhtelafe-Gheymat.sql
@@ -0,0 +1,38 @@
+select 
+
+GETDATE() as DelamMikhadDate,
+product.[Name],
+product.Price as RealPrice,
+invoiceItem.UnitPrice,
+invoice.InvoiceDate,
+invoice.InvoiceNumber
+
+from 
+
+InvoiceItem invoiceItem 
+join Product product on invoiceItem.ProductId = product.id
+join Invoice invoice on invoice.Id = InvoiceItem.InvoiceId
+
+
+where 
+product.Price != invoiceItem.UnitPrice -- and invoice.InvoiceDate < '2023-07-01'
+
+
+
+
+select 
+
+SUM(prod.Price) as SumOfRealPrice,
+SUM(invItem.UnitPrice) as SumOfPrice,
+SUM(prod.Price) - SUM(invItem.UnitPrice) as Diff,
+Count(*) as Cnt
+
+
+from 
+InvoiceItem invItem
+join Product prod on invItem.ProductId = prod.Id
+
+
+
+where 
+invItem.UnitPrice != prod.Price
